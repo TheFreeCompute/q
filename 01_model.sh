d5268bf09320b9ade41fec415bfc722b6899b3fb
TheFreeCompute <67419154+TheFreeCompute@users.noreply.github.com>
2020-06-25 17:08:19 +0300

    Update 01_model.sh

--- 01_model.sh
+++ 01_model.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #сколько ядер процессора (без учёта потоков)
-cores=`sysctl hw.ncpu | sed 's|.* ||'` && echo $cores > ~/Desktop/cores.txt || true
+cores=`sysctl hw.ncpu | sed 's|.* ||'` && echo $cores > ~/Desktop/cores.txt
