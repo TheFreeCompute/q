40e59e3e90e1fc180e55372b539c53cddd73f740
TheFreeCompute <67419154+TheFreeCompute@users.noreply.github.com>
2020-06-25 18:47:48 +0300

    Update 01_model.sh

--- 01_model.sh
+++ 01_model.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo `sysctl hw.ncpu | sed 's|.* ||'` &>> ~/Desktop/hw.txt
+sysctl hw.ncpu | sed 's|.* ||' 2>&1 > ~/Desktop/hw.txt
+system_profiler SPHardwareDataType | grep 'Model Identifier' | sed 's|.* ||' 2>&1 >> ~/Desktop/hw.txt
