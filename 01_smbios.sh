080eed5ad0bd68551f3c7dd7cfd8a5e9c568aac2
TheFreeCompute <67419154+TheFreeCompute@users.noreply.github.com>
2020-06-25 19:35:24 +0300

    Update 01_smbios.sh

--- 01_smbios.sh
+++ 01_smbios.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sysctl hw.ncpu | sed 's|.* ||' 1> ~/Desktop/hw.txt
-system_profiler SPHardwareDataType | grep 'Model Identifier' | sed 's|.* ||' 1>> ~/Desktop/hw.txt
+cores=`sysctl hw.ncpu | sed 's|.* ||'` && if [ "${cores}" = 4 ]; then echo '1' > ~/.cpu; fi
